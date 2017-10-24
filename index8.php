438b4971af802403d07c63101663930786231083
161710151 <161710151@smkassalaambandung.sch.id>
2017-10-24 09:25:05 +0700

    tugas

--- index8.php
+++ index8.php
@@ -5,7 +5,6 @@ class baju {
 	public $ukuran 		= 'xl';
 	public $berbahan 	= 'sutra';
 	public $model		= 'pria';
-	public $harga 		= 25.000;
 
 }
 
@@ -13,8 +12,7 @@ $baju1 = new baju ;
 echo "baju". '<br>' ; 
 echo "	Ukuran baju 		: " .$baju1->ukuran. 	'<br>' ; 
 echo "	Baju berbahan 		: " .$baju1->berbahan. 	'<br>' ;
-echo "	Model baju 			: " .$baju1->model. 	'<br>' ; 
-echo "	Harga 				: " .$baju1->harga. 	'<br>' ;  
+echo "	Model baju 			: " .$baju1->model. 	'<br>' ;   
 
 echo "<br>";
 
